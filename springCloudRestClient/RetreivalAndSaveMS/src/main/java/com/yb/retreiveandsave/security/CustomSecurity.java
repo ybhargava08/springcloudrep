87b05eb176b072fda30a80b16bfe31a4eced5491
ybhargava08 <32349135+ybhargava08@users.noreply.github.com>
2017-10-16 00:32:42 -0400

    Update CustomSecurity.java

--- springCloudRestClient/RetreivalAndSaveMS/src/main/java/com/yb/retreiveandsave/security/CustomSecurity.java
+++ springCloudRestClient/RetreivalAndSaveMS/src/main/java/com/yb/retreiveandsave/security/CustomSecurity.java
@@ -13,7 +13,7 @@ public class CustomSecurity extends WebSecurityConfigurerAdapter {
 	
 	
 	protected void configure(HttpSecurity security) throws Exception{
-        security.authorizeRequests().antMatchers("/","/info","/status","/save","/getLatestData","/getTagCloudData","/fetchDataBasedOnInput/*")
+        security.csrf().disable().authorizeRequests().antMatchers("/","/info","/status","/save","/getLatestData","/getTagCloudData","/fetchDataBasedOnInput/*")
         .permitAll().anyRequest().authenticated().and()
         .httpBasic().and().authorizeRequests().antMatchers("/deleteAll").hasAuthority("ADMIN").anyRequest().authenticated();
 	}
