ca47504399e1eee0faa3e2fa8ac40e488603dd82
ybhargava08 <32349135+ybhargava08@users.noreply.github.com>
2017-11-06 18:02:15 -0500

    Update RetreivalAndSaveController.java

--- springCloudRestClient/RetreivalAndSaveMS/src/main/java/com/yb/retreiveandsave/controller/RetreivalAndSaveController.java
+++ springCloudRestClient/RetreivalAndSaveMS/src/main/java/com/yb/retreiveandsave/controller/RetreivalAndSaveController.java
@@ -2,50 +2,57 @@ package com.yb.retreiveandsave.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.yb.retreiveandsave.service.RetreivalService;
+import com.yb.retreiveandsave.tagidrules.TagIDBean;
 
 import resourcebean.ResourceBean;
 
 @RestController
 public class RetreivalAndSaveController {
 	
-	private static final String APPLICATION_JSON = "application/json";
+	Logger logger = LoggerFactory.getLogger(RetreivalAndSaveController.class);
 	
 	@Autowired
 	private RetreivalService service;
 	
-	@GetMapping(value="/fetchDataBasedOnInput/{input}",produces=APPLICATION_JSON)
-	public List<ResourceBean> fetchDataBasedOnInput(@PathVariable String input){
-		
+	@RequestMapping(value="/fetchDataBasedOnInput",method=RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<ResourceBean> fetchDataBasedOnInput(@RequestBody String input){
+		logger.info("url received from path input : "+input);
 		return service.fetchDataBasedOnInput(input);
 	}
 	
-	@GetMapping(value="/getLatestData",produces=APPLICATION_JSON)
+	@GetMapping(value="/getLatestData",produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResourceBean getLatestData(){
 		return service.getLatestData();
 	}
 	
-	@GetMapping(value="/getTagCloudData",produces=APPLICATION_JSON)
+	@GetMapping(value="/getTagCloudData",produces=MediaType.APPLICATION_JSON_VALUE)
 	public List<ResourceBean> getTagCloudData(){
 		return service.getTagCloudData();
 	}
 	
-	@RequestMapping(value="/save",method=RequestMethod.POST,produces=APPLICATION_JSON,consumes=APPLICATION_JSON)
-	public void saveRequest(@RequestBody ResourceBean resourceBean){
-		 service.saveData(resourceBean);
+	@RequestMapping(value="/save",method=RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE,consumes=MediaType.APPLICATION_JSON_VALUE)
+	public List<ResourceBean> saveRequest(@RequestBody ResourceBean resourceBean){
+		 return service.saveData(resourceBean);
 	}
 	
 	@GetMapping("/deleteAll")
 	public String deleteAll(){
 		service.deleteAllData();
 		return "deletedAll";
+	}
+	
+	@RequestMapping(value="/generateUniqueTagId",method=RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE,consumes=MediaType.APPLICATION_JSON_VALUE)
+	public TagIDBean generateUniqueTagId(@RequestBody ResourceBean bean){
+		return service.generateUniqueTagId(bean);
 	}
 }
